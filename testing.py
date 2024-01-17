2a27609a04c1093d80c20d8f5df960c98eae63ae
lachitooo <39280428+lazarobeas@users.noreply.github.com>
2024-01-17 13:34:01 -0600

    UPDATE

--- testing.py
+++ testing.py
@@ -3,31 +3,36 @@ import pandas as pd
 from nba_api.stats.endpoints import PlayerDashboardByGameSplits, PlayerGameLog
 
 
-capelaid = players.find_players_by_full_name("Anthony Davis")[0]['id']
+capelaid = players.find_players_by_full_name("Bam Adebayo")[0]['id']
 
 print()
 
 
-# player_gamelog = PlayerGameLog(player_id=capelaid, season='2022-23').get_data_frames()[0]
-#
-# column = ['Game_ID', 'GAME_DATE', 'MATCHUP', 'PTS', 'AST', 'STL', 'REB', 'TOV','FGA','FGM', 'FG3M', 'FG3A', 'BLK','FTA','FTM','PLUS_MINUS', ]
-# capelaGameSplits = player_gamelog[column]
-#
-# # Show the resulting dataframe
-# print(capelaGameSplits)
-#
-# df = pd.DataFrame(capelaGameSplits)
-# df.to_csv('C:\\Users\\Lazaro B\\Documents\\GitHub\\NBAProject\\data\\BamAdebayo\\BamAdebayoGameSplits22-23.csv')
+player_gamelog = PlayerGameLog(player_id=capelaid, season='2023-24').get_data_frames()[0]
 
-# df = df.drop(columns=['GAME_DATE', 'MATCHUP']) # Modify this line
-#
+column = ['Game_ID', 'GAME_DATE', 'MATCHUP', 'PTS', 'AST', 'STL', 'REB', 'TOV','FGA','FGM', 'FG3M', 'FG3A', 'BLK','FTA','FTM','PLUS_MINUS', ]
+capelaGameSplits = player_gamelog[column]
+
+# Show the resulting dataframe
+print(capelaGameSplits)
+
+
+
+
+df = pd.DataFrame(capelaGameSplits)
+
+df['PTS+REB+AST'] = df['PTS'] + df['REB'] + df['AST']
+df['PTS+AST'] = df['PTS'] + df['AST']
+df['REB+AST'] = df['REB'] + df['AST']
 # # Add new columns
-# df['PTS+REB'] = df['PTS'] + df['REB']
-# df['PTS+AST'] = df['PTS'] + df['AST']
-# df['PTS+REB+AST'] = df['PTS'] + df['REB'] + df['AST']
-# df['REB+AST'] = df['REB'] + df['AST']
-# df['BLKS+STLS'] = df['BLK'] + df['STL']
-#
-# print(df)
-#
-# df.to_csv('C:\\Users\\Lazaro B\\Documents\\GitHub\\NBAProject\\data\\')
+df['PTS+REB'] = df['PTS'] + df['REB']
+df['BLKS+STLS'] = df['BLK'] + df['STL']
+
+
+df.to_csv('C:\\Users\\Lazaro B\\Documents\\GitHub\\NBAProject\\data\\BamAdebayo\\BamAdebayoGameSplits23-24.csv', index=False)
+
+df = df.drop(columns=['GAME_DATE', 'MATCHUP']) # Modify this line
+
+
+
+print(df)
